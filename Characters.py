61f6b0bfd8372dcccaa1dd491618cff46d8435d5
Orpheus <73367612+Orphyeus@users.noreply.github.com>
2024-02-18 16:24:35 +0300

    Update Characters.py

--- Characters.py
+++ Characters.py
@@ -8,6 +8,7 @@ class Character:
         self.action_type = action_type
         self.alignment = alignment
         self.is_alive = True
+        self.is_protected = False
 
     def perform_action(self, target):
         raise NotImplementedError("Subclass must implement abstract method")
@@ -57,49 +58,71 @@ class Navigator(Character):
             print("Navigator increase the storms probability: ", game_instance.storm_probability)
 
 
-class DiplomaticEnvoy(Character):
-    def __init__(self, name):
-        super().__init__(name, "Diplomatic Envoy", "Aggressive", "Good")
-
-    def perform_action(self, target):
-        # Placeholder for Diplomatic Envoy's secret mission
-        pass
-
-
-class EnvoysGuard(Character):
-    def __init__(self, name):
-        super().__init__(name, "Envoy's Guard", "Aggressive", "Good")
-
-    def perform_action(self, target):
-        # Placeholder for Envoy's Guard's special action
-        pass
+"""EVIL SIDE"""
 
 
 class StormBringer(Character):
     def __init__(self, name):
         super().__init__(name, "StormBringer", "Strategic", "Evil")
-        self.consecutive_kills = 0  # Track consecutive kills
+        self.consecutive_curse = 0  # Track consecutive curse
 
     def perform_action(self, game_instance):
+        game_instance.storm_probability += 0.03
+
         """Increase storm probability after 3 consecutive kills."""
-        if self.consecutive_kills == 3:
-            game_instance.storm_probability += 0.2
+        if self.consecutive_curse == 3:
+            game_instance.storm_probability += 0.1
             print("Storm Bringer increase the storms probability: ", game_instance.storm_probability)
-            self.consecutive_kills = 0  # Reset the counter
+            self.consecutive_curse = 0  # Reset the counter
+
+
+class PirateExecutioner(Character):
+    def __init__(self, name):
+        super().__init__(name, "Pirate Executioner", "Aggressive", "Evil")
+
+    def perform_action(self, target):
+        """Attempt to assassinate the chosen player unless they are protected."""
+        if not target.is_protected:
+            target.is_alive = False
+            print(f"{self.name} has successfully assassinated {target.name}.")
         else:
-            # Reset consecutive kills if no target or target is already dead
-            self.consecutive_kills = 0
-            print(f"{self.name} did not attack anyone this turn.")
+            print(f"{self.name}'s assassination attempt was thwarted. {target.name} is protected.")
 
 
 class Alchemist(Character):
     def __init__(self, name):
         super().__init__(name, "Alchemist", "Strategic", "Evil")
+        self.consecutive_days = 0  # Track consecutive days of compass manipulation
 
     def perform_action(self, game_instance):
-        """Extend the journey by manipulating the compass."""
-        game_instance.journey_days += 1
-        print("Alchemist manipulate the compass, journey day: ", game_instance.journey_days)
+        """Extend the journey by one day if the compass has been manipulated for three consecutive days."""
+        self.consecutive_days += 1  # Increment the count of consecutive days
+        if self.consecutive_days >= 3:  # Check if it's been three consecutive days
+            game_instance.journey_days += 1  # Extend the journey by one day
+            print(f"{self.name} has extended the journey by one day.")
+            self.consecutive_days = 0  # Reset the consecutive days counter
+        else:
+            print(f"{self.name} continues to manipulate the compass.")
+
+
+"""GOOD SIDE"""
+
+
+class Prayer(Character):
+    def __init__(self, name):
+        super().__init__(name, "Prayer", "Strategic", "Good")
+
+    def perform_action(self, game_instance):
+        game_instance.storm_probability -= 0.03
+
+
+class Headhunter(Character):
+    def __init__(self, name):
+        super().__init__(name, "Headhunter", "Aggressive", "Good")
+
+    def perform_action(self, target):
+        """If a pirate enters the room, instantly execute them."""
+        pass
 
 
 class Mercenary(Character):
@@ -107,30 +130,15 @@ class Mercenary(Character):
         super().__init__(name, "Mercenary", "Aggressive", "Neutral")
 
     def perform_action(self, target):
-        # Placeholder for Mercenary's special action
-        pass
-
-
-class Assassin(Character):
-    def __init__(self, name):
-        super().__init__(name, "Assassin", "Aggressive", "Neutral")
-
-    def perform_action(self, target):
-        # Placeholder for Assassin's special action
-        pass
+        target.character.is_protected = True
 
 
 class OrdinaryPassenger(Character):
     def __init__(self, name):
-        super().__init__(name, "Ordinary Passenger", "Aggressive", self.set_random_alignment())
+        super().__init__(name, "Ordinary Passenger", "Aggressive", "Good")
 
     def perform_action(self, target):
-        if self.alignment == "Evil":
-            if target and target.is_alive:
-                target.is_alive = False
-                print(f"{self.name} has killed {target.name}.")
-        else:
-            pass
+        pass
 
 
 class Player:
